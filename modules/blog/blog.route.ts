603b6741d27e5ff71b568e2118f1b48456ed6b0b
akash sunar <sunarakash1122@gmail.com>
2024-02-19 11:09:38 +0545

    feat : CRUD operation of blogs completed

--- modules/blog/blog.route.ts
+++ modules/blog/blog.route.ts
@@ -1,11 +1,18 @@
 import express from "express";
-import { createBlog,getAllBlog,getBlogById,deleteBlogById} from "./blogControllers";
+import {
+  createBlog,
+  getAllBlog,
+  getBlogById,
+  deleteBlogById,
+  updateBlog,
+} from "./blogControllers";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/", createBlog)
-blogRouter.get("/", getAllBlog)
-blogRouter.get("/:id", getBlogById)
-blogRouter.delete("/:id",deleteBlogById)
+blogRouter.get("/", getAllBlog);
+blogRouter.get("/:id", getBlogById);
+blogRouter.post("/", createBlog);
+blogRouter.put("/:id", updateBlog);
+blogRouter.delete("/:id", deleteBlogById);
 
 export default blogRouter;
--- modules/blog/blogControllers.ts
+++ modules/blog/blogControllers.ts
@@ -1,39 +1,49 @@
-import prisma from "../../DB/db.config"
+import prisma from "../../DB/db.config";
 
-export const createBlog = async (req: any, res:any) => {
-    const myBlog = req.body;
-    const newBlog = await prisma.blog.create({
-        data: myBlog,
-    })
-    return res.json({status:200,data:newBlog,msg:"blog created"})
-    
-}
-export const getAllBlog = async (_req:any,res:any) => {
-    const allBlogs = await prisma.blog.findMany({});
-    return res.json({status:200,data:allBlogs,msg:"your all blogs are here"})
-}
+export const createBlog = async (req: any, res: any) => {
+  const myBlog = req.body;
+  const newBlog = await prisma.blog.create({
+    data: myBlog,
+  });
+  return res.status(201).json(newBlog);
+  //   return res.json({ status: 200, data: newBlog, msg: "blog created" });
+};
 
-export const getBlogById = async (req:any,res:any) => {
-    const blogId = parseInt(req.params.id);
-    const blog = await prisma.blog.findUnique({
-        where: {
-            id:blogId
-        }
-    })
-    if (blog) {
-        return res.json({
-            status: 200,
-            data: blog,
-            msg:"your blog is here"
-        })
-    }
-}
-export const deleteBlogById = async (req:any,res:any) => {
-    const blogId = parseInt(req.params.id);
-    await prisma.blog.delete({
-        where: {
-            id:blogId
-        }
-    })
-    res.json({status:200,data:"deleted successfully"})
-}
+export const getAllBlog = async (_req: any, res: any) => {
+  const allBlogs = await prisma.blog.findMany({});
+  return res.status(200).json(allBlogs);
+};
+
+export const updateBlog = async (req: any, res: any) => {
+  const body = req.body;
+  const blogId = parseInt(req.params.id);
+  await prisma.blog.update({
+    where: {
+      id: blogId,
+    },
+    data: { ...body },
+  });
+  return res.status(200).json({ msg: "blog updated succesfully" });
+};
+
+export const getBlogById = async (req: any, res: any) => {
+  const blogId = parseInt(req.params.id);
+  const blog = await prisma.blog.findUnique({
+    where: {
+      id: blogId,
+    },
+  });
+  if (blog) {
+    return res.status(200).json(blog);
+  }
+};
+
+export const deleteBlogById = async (req: any, res: any) => {
+  const blogId = parseInt(req.params.id);
+  await prisma.blog.delete({
+    where: {
+      id: blogId,
+    },
+  });
+  res.json({ status: 200, msg: "deleted successfully" });
+};
