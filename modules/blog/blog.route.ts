b244633c4314d3b88cf520aec67f564c13a69f62
akash sunar <sunarakash1122@gmail.com>
2024-02-19 14:42:16 +0545

    feat : type added in blog.routes

--- modules/blog/blog.route.ts
+++ modules/blog/blog.route.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express,{ Request, Response } from "express";
+import { Blog } from "../blog.type";
 import {
   createBlog,
   getAllBlog,
@@ -9,30 +10,30 @@ import {
 
 const blogRouter = express.Router();
 
-blogRouter.get("/", async (_req: any, res: any) => {
+blogRouter.get("/", async (_req:Request, res: Response):Promise<Response<Blog[]>>=> {
   const allBlogs = await getAllBlog();
-  return res.status(200).json(allBlogs);
+   return res.status(200).json(allBlogs);
 });
 
-blogRouter.get("/:id", async (req: any, res: any) => {
+blogRouter.get("/:id", async (req: Request, res: Response):Promise<Response<Blog[]>> => {
   const blogId = parseInt(req.params.id);
   const blog = await getBlogById(blogId);
-  return res.status(200).json(blog);
+   return res.status(200).json(blog);
 });
 
-blogRouter.post("/", async (req: any, res: any) => {
+blogRouter.post("/", async (req: Request, res:Response):Promise<Response<Blog>>=> {
   const newBlog = await createBlog(req.body);
-  return res.status(201).json(newBlog);
+ return res.status(201).json(newBlog);
 });
 
-blogRouter.put("/:id", async (req: any, res: any) => {
+blogRouter.put("/:id", async (req: Request, res: Response):Promise<Response<Blog>>=> {
   const blogId = parseInt(req.params.id);
   const content = req.body;
   await updateBlog(blogId, content);
   return res.status(201).json({ msg: "Blog updated successfully" });
 });
 
-blogRouter.delete("/:id", async (req: any, res: any) => {
+blogRouter.delete("/:id", async (req: Request, res: Response):Promise<Response<Blog>>=> {
   const blogId = parseInt(req.params.id);
   await deleteBlogById(blogId);
   return res.status(200).json({ msg: "Deleted successfully" });
