e5071e4ebb18413a4ee76c7f79cc10286cc45b69
akash sunar <sunarakash1122@gmail.com>
2024-03-06 16:52:58 +0545

    blog CRUD done by token authorization

---
+++ middlewares/userValidators.ts
@@ -0,0 +1,16 @@
+import { Request, NextFunction, Response } from "express";
+import Jwt, { JwtPayload } from "jsonwebtoken";
+
+export const tokenExtractor = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): Boolean => {
+  const tokenArray = req.headers.authorization?.split(" ");
+  const token = tokenArray ? tokenArray[tokenArray.length - 1] : undefined;
+  const user = Jwt.verify(token || "", process.env.SECRET || "") as JwtPayload;
+  if (!user) throw new Error("user dont exist");
+  req.body.userId = user.id;
+  next();
+  return true;
+};
